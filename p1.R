7c8e6bdcc72a97dd8605f378f81525bff7a170dc
Soumadeep Sarkar <72089988+Soumadeep360@users.noreply.github.com>
2023-12-12 22:09:08 +0530

    Update p1.R

--- p1.R
+++ p1.R
@@ -1,52 +1,41 @@
-# List of products 
-products <- list( 
-  list(name = "Apple", price =120), 
-  list(name = "Banana", price = 35), 
-  list(name = "Milk", price = 25), 
-  list(name = "Bread", price = 50), 
-  list(name = "Eggs", price = 10) 
-) 
-# Initialize shopping cart as an empty list 
-shopping_cart <- list() 
-# Define items to be added to the cart 
-cart_items_to_add <- list( 
-  list(name = "Apple", quantity = 3), 
-  list(name = "Milk", quantity = 2) 
-) 
-# Add items to the shopping cart 
-for (item in cart_items_to_add) { 
-  product_name <- item$name 
-  quantity <- item$quantity 
-  # Find the product in the list 
-  product <- NULL 
-  for (p in products) { 
-    if (p$name == product_name) { 
-      product <- p 
-      break 
-    } 
-  } 
-  if (!is.null(product)) { 
-    cart_item <- list(name = product$name, price = product$price, quantity = 
-                        quantity) 
-    shopping_cart <- c(shopping_cart, list(cart_item)) 
-    cat("Item added to cart.\n") 
-  } else { 
-    cat("Product not found.\n") 
-  } 
-} 
-# Calculate and display receipt 
-subtotal <- 0 
-cat("\nReceipt:\n") 
-for (item in shopping_cart) { 
-  item_subtotal <- item$price * item$quantity 
-  cat(sprintf("%s (%d units) - Price: $%.2f - Subtotal: $%.2f\n", item$name, 
-              item$quantity, item$price, item_subtotal)) 
-  subtotal <- subtotal + item_subtotal 
-} 
-tax_rate <- 0.08 
-tax_amount <- subtotal * tax_rate 
-total_cost_before_tax <- subtotal 
-total_cost <- total_cost_before_tax + tax_amount 
-cat(sprintf("\nSubtotal: $%.2f\n", subtotal)) 
-cat(sprintf("Tax Amount : $%.2f\n", tax_amount)) 
-cat(sprintf("Total Cost: $%.2f\n", total_cost))
+products<-list(
+  list(name="apple",price=123),
+  list(name="mango",price=130),
+  list(name="guava",price=124)
+)
+
+cart_to_be_added<-list(
+  list(name="apple",quantity=3),
+  list(name="mango",quantity=5)
+)
+
+shopping_cart<-list()
+
+for(item in cart_to_be_added){
+  flag<-0
+  for(p in products){
+    if(item$name==p$name){
+      shopping_cart<-c(shopping_cart,list(
+        list(name=item$name,price=p$price,quantity=item$quantity)))
+      flag<-1;
+      break;
+    }
+  }
+  if(flag==1){
+    cat("Item added\n")
+  } else{
+    cat("Product not found\n")
+  }
+}
+subtotal<-0
+cat("Receipt:\n")
+for(item in shopping_cart){
+  cat(sprintf("%s (%d units) Price:$%.2f Subtotal:$$%.2f\n",
+              item$name,item$quantity,item$price,item$price*item$quantity))
+  subtotal<-subtotal+(item$price*item$quantity)
+}
+cat(sprintf("Subtotal:$%.2f",subtotal))
+cat((sprintf("tax_amount:$%.2f",subtotal*0.08)))
+cat(sprintf("Total cost:$%.2f",subtotal+(subtotal*0.08)))
+
+
